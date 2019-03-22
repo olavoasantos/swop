868457fa7b1275642d4dda0fa4b8dae1a496e489
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-03-22 10:49:04 -0500

    SWOP-0: Added github links to the docs and added issues and pr badges

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,13 @@
 export default {
   title: 'Swop',
-  // description: '',
   typescript: true,
   menu: [
     'Main',
+    { name: 'GitHub', menu: [
+      { name: 'Repo', href: 'https://github.com/olavoasantos/swop/' },
+      { name: 'Issues', href: 'https://github.com/olavoasantos/swop/issues' },
+      { name: 'Pull requests', href: 'https://github.com/olavoasantos/swop/pulls' },
+    ] },
   ],
   themeConfig: {
     styles: {
